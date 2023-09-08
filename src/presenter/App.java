6d17fda9374c59eab79ef79e4addbc33bb412fe1
JohanMoraP <133455936+JohanMoraP@users.noreply.github.com>
2023-09-08 11:17:20 -0500

    arreglo MyFrame
    la clase view que estaba la renombré MyFrame y cree el metodo initComponents

--- src/presenter/App.java
+++ src/presenter/App.java
@@ -1,16 +1,17 @@
 package presenter;
 
 import model.Logic;
-import view.View;
+import view.MyFrame;
 
 public class App {
-    private View view;
+    private MyFrame view;
     private Logic logic;
 
     public App() {
-        view = new View();
+        view = new MyFrame();
         logic = new Logic();
     }
+
     public static void main(String[] args) {
         new App();
     }
--- src/view/MyFrame.java
+++ src/view/MyFrame.java
@@ -1,5 +1,6 @@
 package view;
 
+import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -9,31 +10,47 @@ import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import static java.awt.Frame.base;
+
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.Toolkit;
 
-public class View extends JFrame {
-	public View() {
-		Container container = this.getContentPane();
+public class MyFrame extends JFrame {
+	Container container;
+
+	public MyFrame() {
+
+		super("Ciclo de Carnot");
+		Image icon = Toolkit.getDefaultToolkit().getImage("images/imagem.png");
+		setIconImage(icon);
+		this.setMinimumSize(new Dimension(400, 400));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initComponents();
+		this.revalidate();
+		this.pack();
+		this.setVisible(true);
+		this.setExtendedState(MAXIMIZED_BOTH);
+	}
+
+	public void initComponents() {
+		container = this.getContentPane();
 		container.setLayout(new GridBagLayout());
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.fill = GridBagConstraints.BOTH;
 		constraints.gridheight = 3;
 		constraints.weightx = 0.15;
-		container.add(new OptionMenuPanel(), constraints);
+		add(new OptionMenuPanel(), constraints);
 		constraints.weighty = 1.0;
 		constraints.weightx = 0.85;
 		container.add(new PrincipalPanel(), constraints);
-		this.setSize(1000, 700);
-		this.setTitle("Graficadora del ciclo de Carnot");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
 	}
 }
